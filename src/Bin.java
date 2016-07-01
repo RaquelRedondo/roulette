ad6a524cf3d8840589084737abaf8f3a4d5b5139
RaquelRedondo <raquelro22@gmail.com>
2016-07-01 02:31:12 +0100

    Bin class added

--- src/Bin.java
+++ src/Bin.java
@@ -1,4 +1,30 @@
+import java.util.Collection;
+import java.util.Set;
+import java.util.TreeSet;
+
 
 public class Bin {
-
+	Set<Outcome> outcomes = new TreeSet<Outcome>();
+	
+	//TODO Bin constructor
+	Bin(Outcome[] array){
+		
+	}
+	
+	//TODO Bin constructor
+	Bin(Collection<Outcome> collection){
+		
+	}
+	
+	public  void add(Outcome oc){
+		outcomes.add(oc);
+	}
+	
+	public String toString(){
+		StringBuffer outcomeString = new StringBuffer();
+		for (Outcome oc : outcomes){
+			outcomeString.append(oc);
+		}
+		return outcomeString.toString();
+	}
 }
